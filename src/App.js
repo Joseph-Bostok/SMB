0f578db84b63fe821bf93424ffb14a56ea3f3868
Steffen-AG <steffenag@appstate.edu>
2024-07-21 23:06:32 -0400

    server and additude

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/navBar'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ADDitude from './components/pages/ADDitude';
 import Home from './components/pages/home'; 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path='/' exact element={<Home />} /> {/* Updated Route */}
+          <Route path='/additude' element={<ADDitude />} /> {/* ADDitude Route */}
         </Routes>
       </Router>
     </>
